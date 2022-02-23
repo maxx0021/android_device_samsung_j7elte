412e4ed99a37db18c4ca3734971be15e3077dd33
maxx0021 <100127589+maxx0021@users.noreply.github.com>
2022-02-23 19:22:27 +0530

    Update and rename nad_j7elte.mk to colt_j7elte.mk

--- colt_j7elte.mk
+++ colt_j7elte.mk
@@ -20,10 +20,17 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_l_mr1.mk)
 
 # Inherit common Lineage phone.
-$(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+TARGET_BOOT_ANIMATION_RES := 720
+
+# Inherit some common ColtOS stuff.
+$(call inherit-product, vendor/colt/config/common_full_phone.mk)
+ COLT_BUILD_MAINTAINER := Ashwani_02
+
 
 # Set those variables here to overwrite the inherited values.
-PRODUCT_NAME := nad_j7elte
+PRODUCT_NAME := colt_j7elte
 PRODUCT_DEVICE := j7elte
 PRODUCT_MODEL := SM-J700F
 PRODUCT_BRAND := samsung
